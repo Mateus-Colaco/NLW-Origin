82fe4c73580cb137b29085b045a73f1cb5a7ba90
Mateus-Colaco <mateuscol.pereira99@gmail.com>
2021-11-12 12:07:50 -0300

    Desktop Version Finished, Fixed Errors

--- main.js
+++ main.js
@@ -29,7 +29,13 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   pagination: { el: '.swiper-pagination' },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* ScrollReveal */
@@ -58,16 +64,46 @@ scrollReveal.reveal(
 )
 
 /* button-back-to-top */
+
 function backToTop() {
   const backToTopButton = document.querySelector('.back-to-top')
+  backToTopDiv = document.querySelector('.center-button-top')
   if (window.scrollY >= 375) {
     backToTopButton.classList.add('show')
+    backToTopDiv.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
+    backToTopDiv.classList.remove('show')
   }
 }
 
+/*ACTIVE MENU*/
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const topLine = checkpoint >= sectionTop
+    const bottomLine = checkpoint <= sectionTop + sectionHeight
+
+    if (topLine && bottomLine) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/*SCROLL FUNCTIONS*/
 window.addEventListener('scroll', function () {
   backToTop()
   shadowHeaderWhenScroll()
+  activateMenuAtCurrentSection()
 })
